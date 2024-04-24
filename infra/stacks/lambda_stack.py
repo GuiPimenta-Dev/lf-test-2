bf3fa130c27f795639f3c574b2561bb5a3c47029
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-04-24 17:19:21 -0300

    adding pipe docs

---
+++ infra/stacks/lambda_stack.py
@@ -0,0 +1,17 @@
+from docs.config import DocsConfig
+from aws_cdk import Stack
+from constructs import Construct
+from infra.services import Services
+from lambda_forge import release
+
+
+@release
+class LambdaStack(Stack):
+    def __init__(self, scope: Construct, context, **kwargs) -> None:
+
+        super().__init__(scope, f"{context.stage}-{context.name}-Lambda-Stack", **kwargs)
+
+        self.services = Services(self, context)
+
+        # Docs
+        DocsConfig(self.services)
